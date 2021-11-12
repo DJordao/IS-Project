2092e4143f14e331a3069bfc31a36d84ee988b0e
DJordao <diogojordao.2504@gmail.com>
2021-11-12 11:02:18 +0000

    Authenticate

--- jeeapp/ejbs/src/main/java/beans/IBusiness.java
+++ jeeapp/ejbs/src/main/java/beans/IBusiness.java
@@ -1,6 +1,7 @@
 package beans;
 
 import data.BusTrip;
+import data.Users;
 
 import javax.ejb.Remote;
 import java.util.Date;
@@ -10,7 +11,7 @@ import java.util.List;
 public interface IBusiness {
     public void addUser(String email, String nome, String password);
     public void addManager(String email, String nome, String password);
-    public int getUserId(String email);
+    public List<String> authenticate(String email, String password);
     public void editUserInfo(int id, String email, String nome, String password);
     public List<BusTrip> listAvailableTrips(Date dataInicio, Date dataFim);
     public void chargeWallet(int id, float quantia);
---
+++ jeeapp/web/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import beans.IBusiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private IBusiness business;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Logger logger = LoggerFactory.getLogger(LoginServlet.class);
+
+        String email = request.getParameter("Email");
+        String password = request.getParameter("Password");
+
+        String destination = "/error.html";
+        logger.info("ENTREI 1");
+        if (!email.contains(" ") && !password.contains(" ")){
+            //Os campos estao escritos e temos de verificar se estão bem de acordo com a BD
+            List<String> result = business.authenticate(email, password);
+            logger.info("ENTREI 2");
+            if (!result.get(0).equals("Error!")){
+                request.getSession(true).setAttribute("auth", email);
+                destination = "/secured/display.jsp";
+                logger.info("User successfully authenticated!");
+                logger.info(result.get(1));
+            }
+
+        }else{
+            destination = "/error.html";
+            logger.info("Error authenticating user!");
+        }
+        request.getRequestDispatcher(destination).forward(request, response);
+    }
+}
