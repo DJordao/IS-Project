c343ff5d9cfb29a21d368832e78f930ebf3d5511
DJordao <diogojordao.2504@gmail.com>
2021-11-12 11:21:19 +0000

    Merge

---
+++ jeeapp/jpa/src/main/java/data/Ticket.java
@@ -0,0 +1,63 @@
+package data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Ticket implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    private String local;
+
+    @ManyToOne
+    private Users user;
+
+    @ManyToOne
+    private BusTrip viagem;
+
+    public Ticket(){super();}
+
+    public Ticket(Users user, BusTrip viagem, String local) {
+        this.user = user;
+        this.viagem = viagem;
+        this.local = local;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public BusTrip getViagem() {
+        return viagem;
+    }
+
+    public void setViagem(BusTrip viagem) {
+        this.viagem = viagem;
+    }
+}
---
+++ jeeapp/web/src/main/java/servlet/ChargeWalletServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import beans.IBusiness;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/chargeWallet")
+public class ChargeWalletServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    @EJB
+    private IBusiness b;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String value = request.getParameter("Value");
+        String result = "Invalid value.";
+        String destination = "/chargeWalletScreen.html";
+
+        float val = 0;
+        try {
+            val = Float.valueOf(value);
+        } catch (Exception e) {
+            response.getWriter().print(result);
+            request.getRequestDispatcher(destination).forward(request, response);
+        }
+
+        b.chargeWallet(2, val);
+
+        result = "Value added to you account.";
+        response.getWriter().print(result);
+        request.getRequestDispatcher(destination).forward(request, response);
+    }
+}
