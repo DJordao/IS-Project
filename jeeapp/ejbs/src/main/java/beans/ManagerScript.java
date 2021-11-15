434190af53290f8158df0a242d8ebaed2f3d521e
DJordao <diogojordao.2504@gmail.com>
2021-11-15 22:19:43 +0000

    Update

--- jeeapp/ejbs/src/main/java/beans/ManagerScript.java
+++ jeeapp/ejbs/src/main/java/beans/ManagerScript.java
@@ -1,6 +1,8 @@
 package beans;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJBTransactionRolledbackException;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
@@ -9,7 +11,6 @@ import javax.persistence.PersistenceContext;
 import data.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 @Singleton
 @Startup
@@ -22,13 +23,19 @@ public class ManagerScript {
 
     @PostConstruct
     public void initialize() {
+
         Users u = new Users();
-        u.setEmail("manager1@email.com");
+        u.setEmail("diogojordao.2504@gmail.com");
         u.setNome("Manager");
         u.setTipoUser("Manager");
         EncryptData encryptData = new EncryptData();
         String passwordEncrypted = encryptData.encrypt("password");
         u.setPassword(passwordEncrypted);
         em.persist(u);
+    }
+
+    @PreDestroy
+    public void remove() {
+        em.createQuery("delete from Users where email = :email").setParameter("email", "diogojordao.2504@gmail.com").executeUpdate();
     }
 }
